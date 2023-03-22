5437bed4aacb32a51acdeb54f6ee6996b8d5eb96
marpond <sonkoly.marci02@gmail.com>
2023-03-22 12:56:41 +0100

    removed unnecessary swagger header options for better code coverage

--- CineMoviesAPI/Program.cs
+++ CineMoviesAPI/Program.cs
@@ -1,6 +1,3 @@
-using DevOpsCineMovies;
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,11 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CineMoviesAPI", Version = "v1" });
-    c.OperationFilter<AddHeaderBody>();
-});
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(policyBuilder =>
     policyBuilder.AddDefaultPolicy(policy =>
@@ -29,7 +22,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CineMoviesAPI v1"));
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
